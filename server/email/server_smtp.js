22427a71d093e6a59e5a031b6e6d03439a4f2b04
CesarArathAP <122801465+CesarArathAP@users.noreply.github.com>
2024-11-25 22:47:55 -0600

    controlador para el envio de correos

---
+++ server/email/server_smtp.js
@@ -0,0 +1,67 @@
+const isOnlinePromise = import('is-online');
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'cesararath1976@gmail.com',
+        pass: 'xnnrvrmhqmhdbtve'
+    }
+});
+
+let emailQueue = [];
+
+const sendEmail = async (to, subject, html) => {
+    try {
+        // Obtén el módulo is-online después de que esté cargado
+        const isOnline = await isOnlinePromise;
+
+        // Verificar conexión a Internet utilizando is-online
+        const online = await isOnline.default();
+
+        if (online) {
+            await transporter.sendMail({
+                from: 'cesararath1976@gmail.com',
+                to,
+                subject,
+                html
+            });
+            console.log('Correo enviado exitosamente');
+        } else {
+            emailQueue.push({ to, subject, html });
+            console.log('Correo agregado a la cola');
+        }
+    } catch (error) {
+        console.error('Error enviando el correo:', error);
+    }
+};
+
+const processEmailQueue = async () => {
+    try {
+        const isOnline = await isOnlinePromise;
+
+        const online = await isOnline.default();
+
+        if (online && emailQueue.length > 0) {
+            while (emailQueue.length > 0) {
+                const { to, subject, html } = emailQueue.shift();
+                await transporter.sendMail({
+                    from: 'cesararath1976@gmail.com',
+                    to,
+                    subject,
+                    html
+                });
+                console.log('Correo enviado desde la cola');
+            }
+        }
+    } catch (error) {
+        console.error('Error procesando la cola de correos:', error);
+    }
+};
+
+setInterval(processEmailQueue, 60000);
+
+module.exports = sendEmail;
