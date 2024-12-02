934a1022c579fd4a00419bee303c60e19d6255d8
CesarArathAP <122801465+CesarArathAP@users.noreply.github.com>
2024-12-02 13:23:19 -0600

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,11 @@ const db = require('./db/conexion');
 const authRoutes = require('./routes/auth');
 const registerRoutes = require('./routes/register');
 const imageRoutes = require('./routes/imagesprofile');
+const create_group = require('./routes/create_group');
+const join_group = require('./routes/join_group');
+const solicitudes = require('./routes/applications_sent');
+const enviar_message = require('./routes/send_message');
+const calculate_ranking = require('./routes/calculate_rankign');
 // Conectar a la base de datos
 db();
 // Crear instancia de Express
@@ -15,6 +20,11 @@ app.use(express.json());
 // Integrar enrutadores
 app.use('/auth', authRoutes);
 app.use('/api/register', registerRoutes);
+app.use('/api/groups/create', create_group);
+app.use('/api/join/group', join_group);
+app.use('/api/solicitudes/enviadas/grupos', solicitudes);
+app.use('/api/enviar/mensajes/grupos', enviar_message);
+app.use('/api/calculate/ranking/users', calculate_ranking);
 app.use('/api/images', imageRoutes);
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
