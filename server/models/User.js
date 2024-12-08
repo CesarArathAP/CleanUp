dab25239763783d1cd13e08ce75ddcb293c5f905
CesarArathAP <122801465+CesarArathAP@users.noreply.github.com>
2024-12-07 23:44:14 -0600

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -11,11 +11,19 @@ const datosPersonalesSchema = new mongoose.Schema({
   fechaRegistro: { type: Date, default: Date.now }
 }, { _id: false });
 
+// Subesquema para el token
+const tokenSchema = new mongoose.Schema({
+  token: { type: String, required: true },  // Token generado
+  expirationDate: { type: Date, required: true },  // Fecha de expiración del token
+  timestamp: { type: Date, default: Date.now }  // Fecha y hora en que se generó el token
+}, { _id: false });
+
 // Subesquema para credenciales
 const credencialesSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
-  ultimoInicioSesion: { type: Date }
+  ultimoInicioSesion: { type: Date },
+  tokens: [tokenSchema]  // Array de tokens, para almacenar múltiples tokens en caso de cambios
 }, { _id: false });
 
 // Subesquema para actividades del usuario
@@ -29,6 +37,14 @@ const actividadUsuarioSchema = new mongoose.Schema({
   estado: { type: String, default: 'iniciada' }
 }, { _id: false });
 
+// Subesquema para historial de recompensas en el usuario
+const recompensaUsuarioSchema = new mongoose.Schema({
+  cantidad: { type: Number, required: true }, // Cantidad de recompensas otorgadas
+  nombreGrupo: { type: String, required: true }, // Nombre del grupo donde se otorgó la recompensa
+  fecha: { type: Date, default: Date.now },    // Fecha de otorgamiento
+  hora: { type: String, default: () => new Date().toLocaleTimeString('es-MX') } // Hora de otorgamiento
+}, { _id: false });
+
 // Subesquema para actividad
 const actividadSchema = new mongoose.Schema({
   rutasRecorridas: [actividadUsuarioSchema], // Aquí se corrige el arreglo con el subesquema
@@ -36,7 +52,7 @@ const actividadSchema = new mongoose.Schema({
     semana: { type: Date, required: true }, // Inicio de la semana
     puntos: { type: Number, default: 0 }   // Puntos acumulados esa semana
   }],
-  recompensas: [{ type: String }]
+  recompensas: [recompensaUsuarioSchema]
 }, { _id: false });
 
 // Subesquema para grupos
